95d8b8b37535cf7f68da8dd7a4e1ef7aa108370f
Charly <carlos_ds730@yahoo.es>
2016-10-16 01:33:48 -0400

    Working on travel's model

--- travels/models.py
+++ travels/models.py
@@ -17,6 +17,7 @@ class Travel(BaseModelWithStatus):
 
 
 class TravelRequest(BaseModelWithStatus):
+    STATUS_DISMISSED = (-3, 'Dismissed')  # When the taxi's owner said Yes, but the client didn't confirm
     STATUS_CANCELED = (-2, 'Canceled')  # When the client cancels
     STATUS_FAILED = (-1, 'Failed')  # When the taxi owner says No
     STATUS_PENDING_CONFIRMATION_FROM_TAXI_OWNER = (
@@ -26,15 +27,22 @@ class TravelRequest(BaseModelWithStatus):
         3, 'Pending confirmation from the client')  # Waiting for the client to confirm
     STATUS_CONFIRMED = (4, 'Confirmed')  # Both taxi's owner and client has confirmed
 
-    STATUS_CHOICES = [BaseModelWithStatus.STATUS_CREATED, STATUS_FAILED, STATUS_PENDING_CONFIRMATION_FROM_TAXI_OWNER,
+    STATUS_CHOICES = [STATUS_DISMISSED, STATUS_CANCELED, STATUS_FAILED, BaseModelWithStatus.STATUS_CREATED,
+                      STATUS_PENDING_CONFIRMATION_FROM_TAXI_OWNER,
                       STATUS_CONFIRMED_FROM_TAXI_OWNER,
                       STATUS_PENDING_CONFIRMATION_FROM_CLIENT, STATUS_CONFIRMED]
 
     when = models.DateTimeField()
     duration = models.PositiveSmallIntegerField()
     travel = models.OneToOneField(Travel, related_name='request', null=True, blank=True)
+    assigned_car = models.ForeignKey(Car, related_name='travels', null=True, blank=True)
+    assigned_driver = models.ForeignKey(Driver, related_name='travels', null=True, blank=True)
 
 
 class TravelConfirmation(PrimaryModel):
+    # Confirmation message from the taxi's owner.
+    # It must contain the assigned car and the driver.
     travel_request = models.OneToOneField(TravelRequest, related_name='confirmation', null=True, blank=True)
     confirmation_text = models.CharField(max_length=250)
+    assigned_car = models.ForeignKey(Car, related_name='travels', null=True, blank=True)
+    assigned_driver = models.ForeignKey(Driver, related_name='travels', null=True, blank=True)
