9bf27596a2859d63f2a08e1e5d36e7562ba3086d
Charly <carlos_ds730@yahoo.es>
2016-10-30 22:35:46 -0400

    Added the main image model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
+from base.models import PrimaryModel, BaseImage
 from django.db import models
 
-# Create your models here.
+
+def get_next_image_order():
+    last_object = MainImage.objects.order_by('-sort_order').last()
+    if last_object:
+        return last_object.sort_order + 1
+    else:
+        return 1
+
+
+class MainImage(BaseImage):
+    sort_order = models.PositiveSmallIntegerField(verbose_name='Sort order', default=get_next_image_order)
