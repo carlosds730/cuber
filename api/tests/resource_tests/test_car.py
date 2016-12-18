d13aaec6f7fb0cb09b89939bdaf9901c1dedd79a
Charly <carlos_ds730@yahoo.es>
2016-12-17 22:08:52 -0500

    preparing tests for the resource

--- api/tests/resource_tests/test_car.py
+++ api/tests/resource_tests/test_car.py
@@ -1,12 +1,14 @@
-import datetime
 from api.tests import BasicAPITest
+from taxi.models import Car
 
 
 class CarTest(BasicAPITest):
+    RESOURCE_LIST_URI = Car.resource_list_uri()
+
+    # Authentication tests for the resource
     def test_get_list_unauthenticated(self):
-        self.assertHttpOK(self.api_client.get('/api/v1/car/', format='json'))
+        self.assertHttpOK(self.api_client.get(self.RESOURCE_LIST_URI, format='json'))
 
     def test_get_list_authenticated(self):
         self.set_credentials(self.profile)
-        self.assertHttpOK(self.api_client.get('/api/v1/car/', format='json'))
-
+        self.assertHttpOK(self.api_client.get(self.RESOURCE_LIST_URI, format='json'))
--- api/tests/resource_tests/test_travel_request.py
+++ api/tests/resource_tests/test_travel_request.py
@@ -5,13 +5,16 @@ from travels.api import TravelRequestResource
 
 
 class TravelRequestTest(BasicAPITest):
+    RESOURCE_LIST_URI = TravelRequest.resource_list_uri()
+
     # Authentication tests for the resource
     def test_get_list_unauthenticated(self):
-        self.assertHttpUnauthorized(self.api_client.get(TravelRequest.resource_list_uri(), format='json'))
+        self.assertHttpUnauthorized(self.api_client.get(self.RESOURCE_LIST_URI, format='json'))
 
+    # TODO
     def test_get_details_unauthenticated(self):
-        self.assertHttpUnauthorized(self.api_client.get(TravelRequest.resource_list_uri(), format='json'))
+        self.assertHttpUnauthorized(self.api_client.get(self.RESOURCE_LIST_URI, format='json'))
 
     def test_get_list_authenticated(self):
         self.set_credentials(self.profile)
-        self.assertHttpOK(self.api_client.get(TravelRequest.resource_list_uri(), format='json'))
+        self.assertHttpOK(self.api_client.get(self.RESOURCE_LIST_URI, format='json'))
